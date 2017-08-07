8a0a4b75de64b4cd2df2ea4ab7afdc8515d169f3
Jan Brecka <jan.brecka@gmail.com>
2017-08-07 15:33:32 +0200

    init

---
+++ README.md
@@ -0,0 +1,104 @@
+# transmit
+
+A library that brings composition to fetch requests
+
+## Request -> Response
+
+```js
+const req = {
+  url: string,
+  method?: string,
+  headers?: object,
+  body?: any,
+}
+
+const res = {
+  status: number,
+  headers: object,
+  body: any
+}
+```
+
+## Examples
+
+To consume JSON APIs:
+
+```js
+const log = console.log.bind(console)
+
+const fetchJSON = transmit([
+  requestMiddlewares.withBaseUrl('https://example.com/api'),
+  requestMiddlewares.withHeader('Content-Type', 'application/json'),
+  requestMiddlewares.withHeader('Accept', 'application/json'),
+  requestMiddlewares.withEncodedBody(JSON.stringify),
+])([
+  responseMiddlewares.withRetry(),
+  responseMiddlewares.decodeJSONResponse,
+  responseMiddlewares.checkStatus,
+])
+
+fetchJSON({ url: '/foo' }).then(log).catch(log)
+```
+
+To consume transit APIs:
+
+```js
+const transit = require('transit-js')
+const log = console.log.bind(console)
+const writer = transit.writer()
+const reader = transit.reader()
+
+const decodeTransit = (req, res, next) =>
+  next(Object.assign(res, { body: reader.read(res.body) }))
+
+const fetchTransit = transmit([
+  requestMiddlewares.withBaseUrl('https://example.com/api'),
+  requestMiddlewares.withHeader('Content-Type', 'application/json'),
+  requestMiddlewares.withHeader('Accept', 'application/json'),
+  requestMiddlewares.withEncodedBody(writer.write),
+])([
+  responseMiddlewares.withRetry(),
+  responseMiddlewares.decodeTextResponse,
+  responseMiddlewares.checkStatus,
+  decodeTransit,
+])
+
+fetchTransit({ url: '/bar', method: 'post', body: [1, 2, 3] }).then(log).catch(log)
+```
+
+To log each outcoming request (thanks to composition):
+
+```js
+const log = console.log.bind(console)
+
+const tap = (f) => (v) => {
+  f(v)
+  return v
+}
+
+const fetch = transmit([tap(log)])()
+```
+
+## Retries
+
+```js
+responseMiddlewares.withRetry(3, delays.constant())
+// retries after 1 sec, then again after 1 sec, then again after 1 sec
+
+responseMiddlewares.withRetry(3, delays.linear())
+// retries after 1 sec, then after 2 secs, then after 3 secs
+
+responseMiddlewares.withRetry(3, delays.exponential())
+// retries after 1 sec, then after 4 secs, then after 9 secs
+```
+
+## Error handling
+
+Just add `catch` and in case of any error you get the following object:
+
+```js
+err.message
+err.original
+err.req = req
+err.res = res
+```
---
+++ index.js
@@ -0,0 +1,119 @@
+const fetch = require('isomorphic-fetch')
+
+const pipe = (...fns) => (value) => fns.reduce((r, f) => f(r), value)
+
+const delay = (time) => new Promise((resolve, _) => { setTimeout(() => resolve(), time) })
+
+const requestMiddlewares = {
+
+  withBaseUrl: (baseUrl) => (req) => {
+    return Object.assign(req, { url: baseUrl + req.url })
+  },
+
+  withHeader: (header, value) => (req) => {
+    return Object.assign(req, { headers: Object.assign(req.headers || {}, { [header]: value }) })
+  },
+
+  withEncodedBody: (encoder) => (req) => {
+    if ((req.method && req.method.toLowerCase() !== 'get') && req.body)
+      return Object.assign(req, { body: encoder(req.body) })
+    return req
+  },
+
+}
+
+const delays = {
+  constant: (time = 1000) => (i) => delay(time),
+  linear: (time = 1000) => (i) => delay(i * time),
+  exponential: (time = 1000) => (i) => delay(i * i * time)
+}
+
+const responseMiddlewares = {
+
+  decodeResponse: async (req, res, next) => {
+    try {
+      const body = await res[(res.headers.get('content-type') || '').indexOf('application/json') === 0 ? 'json' : 'text']()
+      next(Object.assign(res, { body }))
+    } catch (e) {
+      next(e)
+    }
+  },
+
+  decodeTextResponse: async (req, res, next) => {
+    try {
+      const body = await res.text()
+      next(Object.assign(res, { body }))
+    } catch (e) {
+      next(e)
+    }
+  },
+
+  decodeJSONResponse: async (req, res, next) => {
+    try {
+      const body = await res.json()
+      next(Object.assign(res, { body }))
+    } catch (e) {
+      next(e)
+    }
+  },
+
+  checkStatus: (req, res, next) => {
+    if (res.status < 200 || res.status >= 400)
+      return next(new Error('Invalid status code.'))
+
+    next(res)
+  },
+
+  withRetry: (max = 5, delay = delays.linear()) => (req, res, next) => {
+    if (res.status >= 200) {
+      if (!req.retry || req.retry < max) {
+        const retry = (req.retry || 0) + 1
+        delay(retry).then(() => next(Object.assign(req, { retry }), res))
+      } else {
+        next(new Error('Retry failed.'))
+      }
+
+      return
+    }
+
+    next(res)
+  },
+
+}
+
+const transmit = (requestMiddlewares = []) => (responseMiddlewares = []) => (req) => {
+  const reqM = pipe.apply(null, requestMiddlewares)(req)
+
+  return fetch(reqM.url, reqM).then((res) => new Promise((resolve, reject) => {
+
+    const transmitError = (e, req, res) => {
+      const err = new Error(e.message)
+      err.original = e
+      err.req = req
+      err.res = res
+      return err
+    }
+
+    const next = ([head, ...tail], req, res) => {
+      if (!head) return res
+      head(req, res, (...args) => {
+        if (args.length === 1 && args[0] instanceof Error) reject(transmitError(args[0], req, res))
+        else if (args.length === 1) next(tail, req, args[0])
+        else if (args.length === 2) next([head].concat(tail), args[0], args[1])
+        else reject(transmitError(new Error('next function called with wrong arity')))
+      })
+    }
+
+    next(responseMiddlewares.concat((req, res) => resolve(res)), reqM, res)
+  }))
+}
+
+module.exports = {
+  fetch,
+  pipe,
+  delay,
+  delays,
+  requestMiddlewares,
+  responseMiddlewares,
+  transmit,
+}
