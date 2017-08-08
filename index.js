4c12ca11b5e0d08bcbd10791d6fb4e8ea73ff9ad
Jan Brecka <jan.brecka@gmail.com>
2017-08-08 11:35:47 +0200

    composable

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
-const fetch = require('isomorphic-fetch')
+const $fetch = require('isomorphic-fetch')
 
-const pipe = (...fns) => (value) => fns.reduce((r, f) => f(r), value)
+const isPromise = (v) => !!v
+  && (typeof v === 'object' || typeof v === 'function')
+  && typeof v.then === 'function'
 
-const delay = (time) => new Promise((resolve, _) => { setTimeout(() => resolve(), time) })
+const pipeP = (...fns) => (value) => {
+  return new Promise((resolve, reject) => {
+    const run = ([f, ...fns]) => (value) => {
+      console.log('pipeK:', f, value)
+      if (!f) resolve(value)
+      else {
 
-const requestMiddlewares = {
-
-  withBaseUrl: (baseUrl) => (req) => {
-    return Object.assign(req, { url: baseUrl + req.url })
-  },
-
-  withHeader: (header, value) => (req) => {
-    return Object.assign(req, { headers: Object.assign(req.headers || {}, { [header]: value }) })
-  },
-
-  withEncodedBody: (encoder) => (req) => {
-    if ((req.method && req.method.toLowerCase() !== 'get') && req.body)
-      return Object.assign(req, { body: encoder(req.body) })
-    return req
-  },
-
+        value = f(value)
+        if (isPromise(value)) value.then(run(fns)).catch(reject)
+        else run(fns)(value)
+      }
+    }
+    run(fns)(value)
+  })
 }
+
+const delay = (time) => new Promise((resolve, _) => {
+  setTimeout(() => resolve(), time)
+})
+
+const delayedFail = (timeout) => delay(timeout).then(() => {
+  throw new Error('Timeout')
+})
 
 const delays = {
   constant: (time = 1000) => (i) => delay(time),
@@ -28,108 +34,77 @@ const delays = {
   exponential: (time = 1000) => (i) => delay(i * i * time)
 }
 
-const responseMiddlewares = {
+const fetch = (req) => $fetch(req.url, req)
 
-  withSafe204: (text = '', json = {}) => (req, res, next) => {
-    if (res.status === 204) {
-      next(Object.assign(res, {
-        text: () => Promise.resolve(text),
-        json: () => Promise.resolve(json)
-      }))
-      return
-    }
+const retryableFetch = (req) => () => fetch(req)
 
-    next(res)
-  },
-
-  decodeResponse: async (req, res, next) => {
-    try {
-      const body = await res[(res.headers.get('content-type') || '').indexOf('application/json') === 0 ? 'json' : 'text']()
-      next(Object.assign(res, { body }))
-    } catch (e) {
-      next(e)
-    }
-  },
-
-  decodeTextResponse: async (req, res, next) => {
-    try {
-      const body = await res.text()
-      next(Object.assign(res, { body }))
-    } catch (e) {
-      next(e)
-    }
-  },
-
-  decodeJSONResponse: async (req, res, next) => {
-    try {
-      const body = await res.json()
-      next(Object.assign(res, { body }))
-    } catch (e) {
-      next(e)
-    }
-  },
-
-  checkStatus: (req, res, next) => {
-    if (res.status < 200 || res.status >= 400)
-      return next(new Error('Invalid status code.'))
-
-    next(res)
-  },
-
-  withRetry: (max = 5, delay = delays.linear()) => (req, res, next) => {
-    if (res.status >= 500) {
-      if (!req.retry || req.retry < max) {
-        const retry = (req.retry || 0) + 1
-        delay(retry).then(() => next(Object.assign(req, { retry }), res))
-      } else {
-        next(new Error('Retry failed.'))
-      }
-
-      return
-    }
-
-    next(res)
-  },
-
-  tap: (f) => (req, res, next) => {
-    f(req, res)
-    next(res)
-  }
-
+const withBaseUrl = (baseUrl) => (req) => {
+  req.url = baseUrl + req.url
+  return req
 }
 
-const composableFetch = (requestMiddlewares = []) => (responseMiddlewares = []) => (req) => {
-  const reqM = pipe.apply(null, requestMiddlewares)(req)
+const withHeader = (header, value) => (req) => {
+  req.headers = req.headers || {}
+  req.headers[header.toLowerCase()] = value
+  return req
+}
 
-  return fetch(reqM.url, reqM).then((res) => new Promise((resolve, reject) => {
+const withEncodedBody = (encoder) => (req) => {
+  if ((req.method && req.method.toLowerCase() !== 'get') && req.body)
+    req.body = encoder(req.body)
+  return req
+}
 
-    const error = (e, req, res) => {
-      const err = new Error(e.message)
-      err.original = e
-      err.req = req
-      err.res = res
-      return err
-    }
+const withSafe204 = (text = '', json = {}) => (res) => {
+  if (res.status === 204) {
+    res.text = () => Promise.resolve(text),
+    res.json = () => Promise.resolve(json)
+  }
+  return res
+}
 
-    const next = ([head, ...tail], req, res) => {
-      if (!head) return res
-      head(req, res, (...args) => {
-        if (args.length === 1 && args[0] instanceof Error) reject(error(args[0], req, res))
-        else if (args.length === 1) next(tail, req, args[0])
-        else if (args.length === 2) next([head].concat(tail), args[0], args[1])
-        else reject(error(new Error('next function called with wrong arity')))
+const decodeResponse = async (res) => {
+  const body = await res[(res.headers.get('content-type') || '').indexOf('application/json') === 0 ? 'json' : 'text']()
+  res.body = body
+  return res
+}
+
+const checkStatus = (res) => {
+  if (res.status < 200 || res.status >= 400)
+    throw new Error('Invalid status code.')
+  return res
+}
+
+const withTimeout = (timeout) => (retryableFetch) => {
+  return () => Promise.race([retryableFetch(), delayedFail(timeout)])
+}
+
+const withRetry = (max = 5, delay = delays.linear()) => (retryableFetch) => {
+  return new Promise((resolve, reject) => {
+    const run = (t) => {
+      if (t === max) return reject(new Error('Retry failed'))
+      retryableFetch().then(resolve).catch((e) => {
+        delay(t).then(() => run(t + 1))
       })
     }
-
-    next(responseMiddlewares.concat((req, res) => resolve(res)), reqM, res)
-  }))
+    run(0)
+  })
 }
 
 module.exports = {
-  pipe,
+  pipeP,
   delay,
   delays,
-  requestMiddlewares,
-  responseMiddlewares,
-  fetch: composableFetch,
+  composableFetch: {
+    fetch,
+    retryableFetch,
+    withBaseUrl,
+    withHeader,
+    withEncodedBody,
+    withSafe204,
+    decodeResponse,
+    checkStatus,
+    withTimeout,
+    withRetry,
+  },
 }
