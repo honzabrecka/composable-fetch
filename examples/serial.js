c9b990fd95879fd151d1227180a752687abefe38
Jan Brecka <jan.brecka@gmail.com>
2017-08-08 16:20:28 +0200

    serial-fetch example

---
+++ examples/serial.js
@@ -0,0 +1,24 @@
+const { composableFetch, pipeP } = require('../index')
+const log = console.log.bind(console)
+
+// this will run request one by one
+// for more info check https://github.com/honzabrecka/serial-fetch
+const $serialFetch = require('serial-fetch').default
+const serialFetch = $serialFetch(composableFetch.fetch)
+const retryableSerialFetch = (req) => () => serialFetch(req)
+
+const fetchJSON = pipeP(
+  composableFetch.withBaseUrl('https://honzabrecka.com/api'),
+  composableFetch.withHeader('Content-Type', 'application/json'),
+  composableFetch.withHeader('Accept', 'application/json'),
+  composableFetch.withEncodedBody(JSON.stringify),
+  retryableSerialFetch,
+  composableFetch.withTimeout(6000),
+  composableFetch.withRetry(),
+  composableFetch.withSafe204(),
+  composableFetch.decodeResponse,
+  composableFetch.checkStatus,
+)
+
+fetchJSON({ url: '/aletta/status/201?sleep=5' }).then(log).catch(log)
+fetchJSON({ url: '/aletta/status/200?sleep=2' }).then(log).catch(log)
