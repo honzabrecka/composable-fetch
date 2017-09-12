369006ae9065cf1ba33c7ccd2ad917780e26b311
Jan Brecka <jan.brecka@gmail.com>
2017-09-12 14:53:04 +0200

    generated test for pipeP

--- __tests__/pipeP.spec.ts
+++ __tests__/pipeP.spec.ts
@@ -1,10 +1,37 @@
-import { pipeP } from '../index'
+import { asyncForAll, generators } from 'rapid-check'
+import { delay, pipeP } from '../index'
 
 const add5 = (n) => n + 5
 const rem3 = (n) => n - 3
 const toPromise = (f) => (v) => Promise.resolve(f(v))
 
 describe('pipeP', () => {
+  it('tests pipeP', async () => {
+    const gen = generators.array(generators.tuple(
+      // to check that composition is in right order '3' + '2' == '32'
+      generators.choose(0, 9),
+      // delayed? == fn returns promise
+      generators.bool,
+      // when delayed, then how much?
+      generators.choose(0, 25),
+    ), 0, 20)
+
+    const prop = async (vs) => {
+      const toFn = ([n, delayed, time]) => delayed
+        ? (p) => delay(time).then((_) => p + n)
+        : (p) => p + n
+
+      const empty = 'x'
+      const a = await pipeP.apply(null, vs.map(toFn))(empty)
+      const b = vs.map(([n]) => n).reduce((a, b) => a + b, empty)
+
+      return a === b
+    }
+
+    const [result] = await asyncForAll(gen, prop)
+    expect(result).toBe(true)
+  }, 25000)
+
   it('returns promise with untouched value for empty chain', async () => {
     const value = { foo: 'bar' }
     const result = await pipeP()(value)
