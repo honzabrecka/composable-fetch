26a1910b82296b01ac9edcb0ddfb2a8c51db6719
Jan Brecka <jan.brecka@gmail.com>
2018-08-21 13:03:27 +0200

    no, it does not work

--- flow-test.js
+++ flow-test.js
@@ -3,23 +3,14 @@
 import { pipeP, tryCatchP } from './index'
 import type { UnaryFn, UnaryFnP } from './index'
 
-const a = (x: number): string => x + ''
-const b = (x: string): number => parseInt(x)
-const aP = (x: number): Promise<string> => Promise.resolve(x + '')
-const bP = (x: string): Promise<number> => Promise.resolve(parseInt(x))
+const ns = (x: number): string => x + ''
+const sn = (x: string): number => parseInt(x)
+const nsP = (x: number): Promise<string> => Promise.resolve(x + '')
+const snP = (x: string): Promise<number> => Promise.resolve(parseInt(x))
 
-pipeP(a, b)(1)
-pipeP(aP, bP)(1)
-pipeP(aP, b)(1)
-pipeP(a, bP)(1)
-
-const x: Promise<string> = tryCatchP(aP, () => 'foo')(1)
-
-declare type Comp<A, B, C> = (f: UnaryFn<A, B>, g: UnaryFn<B, C>) => UnaryFnP<A, C>
-
-const comp: Comp<*, * , *> = (f, g) => (v) => (g((f((v: any)): any)): any)
-
-comp(a, b)(1)
-comp(aP, bP)(1)
-comp(aP, b)(1)
-comp(a, bP)(1)
+const okPipes: Promise<number>[] = [
+  pipeP(ns, sn)(1),
+  pipeP(nsP, snP)(1),
+  pipeP(nsP, sn)(1),
+  pipeP(ns, snP)(1),
+]
