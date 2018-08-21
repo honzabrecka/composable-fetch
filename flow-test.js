fa4d0fb59369c03a83678c8086be27399f7c8869
Jan Brecka <jan.brecka@gmail.com>
2018-08-21 13:23:35 +0200

    no

--- flow-test.js
+++ flow-test.js
@@ -1,12 +1,12 @@
 // @flow
 
-import { pipeP, tryCatchP } from './index'
+import { pipeP, tryCatchP, composableFetch } from './index'
 import type { UnaryFn, UnaryFnP } from './index'
 
-const ns = (x: number): string => x + ''
-const sn = (x: string): number => parseInt(x)
-const nsP = (x: number): Promise<string> => Promise.resolve(x + '')
-const snP = (x: string): Promise<number> => Promise.resolve(parseInt(x))
+const ns: UnaryFn<number, string> = (x: number): string => x + ''
+const sn: UnaryFn<string, number> = (x: string): number => parseInt(x)
+const nsP: UnaryFn<number, string> = (x: number): Promise<string> => Promise.resolve(x + '')
+const snP: UnaryFn<string, number> = (x: string): Promise<number> => Promise.resolve(parseInt(x))
 
 const okPipes: Promise<number>[] = [
   pipeP(ns, sn)(1),
@@ -14,3 +14,16 @@ const okPipes: Promise<number>[] = [
   pipeP(nsP, sn)(1),
   pipeP(ns, snP)(1),
 ]
+
+const fetchJSON = pipeP(
+  composableFetch.withBaseUrl('https://example.com/api'),
+  composableFetch.withHeader('Content-Type', 'application/json'),
+  composableFetch.withHeader('Accept', 'application/json'),
+  composableFetch.withEncodedBody(JSON.stringify),
+  composableFetch.retryable(composableFetch.fetch1(fetch)),
+  composableFetch.withTimeout(1000),
+  composableFetch.withRetry(),
+  composableFetch.withSafe204(),
+  composableFetch.decodeJSONResponse,
+  composableFetch.checkStatus
+)
